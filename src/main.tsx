90992bc51ef2c08c731ce23d2f429ed6bba47e74
Ignacio Revainera <ignacio.revainera@hotmail.com>
2026-08-04 20:26:02 -0300

    feat: wrap app with NotificationProvider and Modal

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import CommandView from "./CommandView";
 import ErrorBoundary from "./components/ErrorBoundary";
+import { NotificationProvider } from "./contexts/NotificationContext";
+import NotificationModal from "./components/NotificationModal";
 import { initializeSettings } from "./utils/settings";
 
 initializeSettings();
@@ -14,7 +16,10 @@ const isPopup = params.has("type");
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <ErrorBoundary>
-      {isPopup ? <CommandView /> : <App />}
+      <NotificationProvider>
+        {isPopup ? <CommandView /> : <App />}
+        <NotificationModal />
+      </NotificationProvider>
     </ErrorBoundary>
   </StrictMode>,
 );
